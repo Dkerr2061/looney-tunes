5fa696f0eef55fc6551e5eadc630a812fbd42f71
David Kerr <kerrky@gmail.com>
2025-01-14 20:45:40 -0500

    Fixed home page bug

--- src/pages/AddArtist.tsx
+++ src/pages/AddArtist.tsx
@@ -10,21 +10,23 @@ function AddArtist() {
   };
 
   return (
-    <form className="flex flex-col items-center w-72">
-      <label>Add Artist Name</label>
-      <input type="text" />
-      <label>Add Artist Image</label>
-      <input type="text" />
-      <label>Please Select Tags</label>
-      <CustomTagSelect
-        selectedTags={selectedTags}
-        onTagChange={handleTagChange}
-      />
-      <Tags tags={selectedTags} />
-      <button type="submit" className="mt-4 btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <div className="flex justify-center border-2 border-black w-80">
+      <form className="flex flex-col items-center w-72">
+        <label>Add Artist Name</label>
+        <input type="text" />
+        <label>Add Artist Image</label>
+        <input type="text" />
+        <label>Please Select Tags</label>
+        <CustomTagSelect
+          selectedTags={selectedTags}
+          onTagChange={handleTagChange}
+        />
+        <Tags tags={selectedTags} />
+        <button type="submit" className="mt-4 btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
